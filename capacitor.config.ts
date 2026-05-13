62a2412a4d065a03c78e0421e84fb3e246f2641a
YehinnerTA <ytorresastorga@gmail.com>
2026-05-13 13:41:55 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'delicias_ali',
+  webDir: 'dist'
+};
+
+export default config;
